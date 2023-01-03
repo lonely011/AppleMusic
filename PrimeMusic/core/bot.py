8f13263cc19deccee12488c07d510e6bfe0775be
lonely011 <120551291+lonely011@users.noreply.github.com>
2023-01-03 13:41:51 +0700

    Update bot.py

--- PrimeMusic/core/bot.py
+++ PrimeMusic/core/bot.py
@@ -20,7 +20,7 @@ class PrimeBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot")
         super().__init__(
-            "PrimeMusicBot",
+            "AppleMusicBot",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
