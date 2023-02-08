67b2ccec650c973cc242f8c3badad8943fa3ed80
lonely011 <120551291+lonely011@users.noreply.github.com>
2023-02-08 20:36:49 +0700

    Update start.py

--- PrimeMusic/utils/inline/start.py
+++ PrimeMusic/utils/inline/start.py
@@ -97,7 +97,6 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         buttons.append(
             [
                 InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
-                ),
             ]
         )
     else:
